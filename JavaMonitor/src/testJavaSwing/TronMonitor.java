7f4bd736eec3d3c2c71cd90989fbbbeb631f9bea
Nicolas Harrand <nicolas.harrand@gmail.fr>
2015-04-27 08:10:15 +0200

    add a full java working prototype for monitor

--- JavaMonitor/src/testJavaSwing/TronMonitor.java
+++ JavaMonitor/src/testJavaSwing/TronMonitor.java
@@ -4,15 +4,26 @@ package testJavaSwing;
 import javax.swing.SwingUtilities;
 
 public class TronMonitor {
+	public TronFrame fenetre;
+	
 	public static void main(String[] args){
 		
-		TronFrame fenetre = new TronFrame();
+		fenetre = new TronFrame();
 		fenetre.setVisible(true);
 		fenetre.paintSquare(0, 0, 0);
 		fenetre.paintSquare(1, 0, 1);
 		fenetre.paintSquare(0, 1, 2);
 		fenetre.paintSquare(1, 1, 0);
 		
+	}
+	
+	public TronMonitor() {
+		fenetre = new TronFrame();
+		fenetre.setVisible(true);
+	}
+	
+	public void paintSquare(int x, int y, int id) {
+		fenetre.paintSquare(x, y, id);
 	}
 	
 }
